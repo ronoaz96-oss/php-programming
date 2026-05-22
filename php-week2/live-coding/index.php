7e0235ce91b384d18a1f0fc1d5d2cdf0c35f2d8e
ronoaz96-oss <ronoaz96@gmail.com>
2026-05-22 09:31:15 +0700

    firs commit

--- php-week2/live-coding/index.php
+++ php-week2/live-coding/index.php
@@ -1,1 +1,10 @@
 <h1>Live Coding</h1>
+<?php 
+echo "<div style='weight:1440px; justify:display: flex; justify-content: center; ' >
+
+        <h1> Dashboard</h1>
+        <h3> jfdjksadjfkhkjfhjksahfj</h3>
+
+     </div>";
+
+?>
---
+++ test.php
@@ -0,0 +1,43 @@
+<?php 
+
+$name = "Kong panhapech";
+
+echo "
+<div style='
+    width:1440px;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    
+    
+    height:300px;
+'>
+
+    <div style='
+    
+    background-color:blue;
+    
+    padding-top:30px;
+    padding-left: 60px;
+    padding-right: 60px;
+    height:300px;
+    color:white;
+    text-align:center;
+     align-items:center;
+     justify-content:center;
+     border-radius: 20px;
+'>
+    
+        <h1>Dashboard</h1>
+
+    <h3>
+        My name is <span style='color:black; font-size:bold;'>  $name</span> , I am a student in WMAD C-1 class <br>
+        I want to be a full-stack developer 
+    </h3>
+    </div>
+
+</div>
+";
+
+?>
